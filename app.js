c60e3165b8b297d3b6ecd486b8933ad9b575f896
stainpl <3tyokaha@gmail.com>
2025-11-19 09:26:47 +0100

    added homepage

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.use('/api/applications', require('./routes/applications'));
 app.use('/api/notifications', require('./routes/notifications'));
 app.use('/api/admin', require('./routes/admin'));
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
